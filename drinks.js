dc31c8491f3311fc9189850d8be0a50e8d672ce5
Ian Cristino <2224378@slu.edu.ph>
2023-10-30 23:01:53 +0800

    Fixed errors

--- drinks.js
+++ drinks.js
@@ -1,13 +1,13 @@
-const searchBtn = document.getElementById('search-btn');
+const searchBtn = document.getElementById('drink-search-btn');
 const drinkList = document.getElementById('drink');
 const drinkDetailsContent = document.querySelector('.drink-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const recipeCloseBtn = document.getElementById('drink-recipe-close-btn');
 const drinkPage = document.getElementById('drink-container');
 
 
 // event listeners
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
+searchBtn.addEventListener('click', getDrinkList);
+drinkList.addEventListener('click', getDrinkRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
@@ -20,4 +20,14 @@ function getDrinkList(){
         let html = "";
 
     })
+}
+
+function getDrinkRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
 }

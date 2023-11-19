92694dd91f7ea5eef4c9e9d68cc74fcb3e30197b
Ian Cristino <2224378@slu.edu.ph>
2023-11-19 10:23:01 +0800

    refactored script.js beautified it, shortened it

--- script.js
+++ script.js
@@ -4,175 +4,110 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 const categoryDropdown = document.getElementById('categoryDropdown');
 
-// event listeners
+// Event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 categoryDropdown.addEventListener('change', getMealList);
-recipeCloseBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showRecipe');
-});
+recipeCloseBtn.addEventListener('click', () => mealDetailsContent.parentElement.classList.remove('showRecipe'));
 
+// Fetch categories and populate the dropdown
 fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-      .then(response => response.json())
-      .then(data => {
-        // Populate the dropdown with categories
-        data.categories.forEach(category => {
-          const option = document.createElement('option');
-          option.value = category.strCategory;
-          option.text = category.strCategory;
-          categoryDropdown.add(option);
-        });
-      })
-      .catch(error => console.error('Error fetching categories:', error));
+    .then(response => response.json())
+    .then(data => populateDropdown(data.categories))
+    .catch(error => console.error('Error fetching categories:', error));
 
-// get meal list that matches with the ingredients
+// Function to populate the dropdown with categories
+function populateDropdown(categories) {
+    categories.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category.strCategory;
+        option.text = category.strCategory;
+        categoryDropdown.add(option);
+    });
+}
+
+// Function to get meal list based on ingredients and category
 function getMealList() {
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    let selectedCategory = categoryDropdown.value; // Get the selected category
+    const searchInputTxt = document.getElementById('search-input').value.trim();
+    const selectedCategory = categoryDropdown.value;
 
-    // Use the selected category in the API request
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}&c=${selectedCategory}`)
         .then(response => response.json())
         .then(data => {
-            let html = "";
-            if (data.meals) {
-                data.meals.forEach(meal => {
-                    html += `
-                        <div class="meal-item" data-id="${meal.idMeal}">
-                            <div class="meal-img">
-                                <img src="${meal.strMealThumb}" alt="food">
-                            </div>
-                            <div class="meal-name">
-                                <h3>${meal.strMeal}</h3>
-                                <a href="#" class="recipe-btn">Get Recipe</a>
-                            </div>
+            let html = data.meals
+                ? data.meals.map(meal => `
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="food">
                         </div>
-                    `;
-                });
-                mealList.classList.remove('notFound');
-            } else {
-                html = "Sorry, we didn't find any meal!";
-                mealList.classList.add('notFound');
-            }
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>`
+                ).join('')
+                : "Sorry, we didn't find any meal!";
 
             mealList.innerHTML = html;
+            mealList.classList.toggle('notFound', !data.meals);
         });
 }
 
-
-// get recipe of the meal
-function getMealRecipe(e){
+// Function to get recipe of the meal
+function getMealRecipe(e) {
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;
+    if (e.target.classList.contains('recipe-btn')) {
+        const mealItem = e.target.parentElement.parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+            .then(response => response.json())
+            .then(data => mealRecipeModal(data.meals[0]));
     }
 }
 
-// create a modal
-function mealRecipeModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let steps = meal.strInstructions.split(".");
-    let ingredients = getIngredients(meal);
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Ingredients: </h3>
-                <ul id="food-ingredients-list">
-                </ul>
-            <h3>Instructions:</h3>
-                <ol id="food-steps-list">
-                </ol>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    mealDetailsContent.innerHTML = html;
-    let stepsList = mealDetailsContent.querySelector("ol");
-    let ingredientsList = mealDetailsContent.querySelector("ul");
-    for (let step of steps) {
-        if (step == "") continue;
-        else {
-            let stepEl = document.createElement("li");
-            stepEl.textContent = step;
-            stepsList.appendChild(stepEl);
-        }
-    }
+// Function to create a modal
+function mealRecipeModal(meal) {
+    const steps = meal.strInstructions.split(".");
+    const ingredients = getIngredients(meal);
 
-    for (let ingredient of ingredients) {
-        if (ingredient == "") continue;
-        else {
-            let ingredientEl = document.createElement("li");
-            ingredientEl.textContent = ingredient;
-            ingredientsList.appendChild(ingredientEl);
-        }
-    }
+    const html = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Ingredients: </h3>
+            <ul id="food-ingredients-list"></ul>
+            <h3>Instructions:</h3>
+            <ol id="food-steps-list"></ol>
+        </div>
+        <div class="recipe-meal-img">
+            <img src="${meal.strMealThumb}" alt="">
+        </div>
+        <div class="recipe-link">
+            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+        </div>`;
+
+    mealDetailsContent.innerHTML = html;
+    const stepsList = mealDetailsContent.querySelector("#food-steps-list");
+    const ingredientsList = mealDetailsContent.querySelector("#food-ingredients-list");
+
+    steps.forEach(step => step && stepsList.appendChild(createListItem(step)));
+    ingredients.forEach(ingredient => ingredient && ingredientsList.appendChild(createListItem(ingredient)));
+
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
 
+// Function to get ingredients
 function getIngredients(meal) {
-    let ingredients = [];
-    let measures = [];
-    ingredients.push(meal.strIngredient1);
-    ingredients.push(meal.strIngredient2);
-    ingredients.push(meal.strIngredient3);
-    ingredients.push(meal.strIngredient4);
-    ingredients.push(meal.strIngredient5);
-    ingredients.push(meal.strIngredient6);
-    ingredients.push(meal.strIngredient7);
-    ingredients.push(meal.strIngredient8);
-    ingredients.push(meal.strIngredient9);
-    ingredients.push(meal.strIngredient10);
-    ingredients.push(meal.strIngredient11);
-    ingredients.push(meal.strIngredient12);
-    ingredients.push(meal.strIngredient13);
-    ingredients.push(meal.strIngredient14);
-    ingredients.push(meal.strIngredient15);
-    ingredients.push(meal.strIngredient16);
-    ingredients.push(meal.strIngredient17);
-    ingredients.push(meal.strIngredient18);
-    ingredients.push(meal.strIngredient19);
-    ingredients.push(meal.strIngredient20);
+    const ingredients = Array.from({ length: 20 }, (_, i) => meal[`strIngredient${i + 1}`]);
+    const measures = Array.from({ length: 20 }, (_, i) => meal[`strMeasure${i + 1}`]);
 
-    measures.push(meal.strMeasure1);
-    measures.push(meal.strMeasure2);
-    measures.push(meal.strMeasure3);
-    measures.push(meal.strMeasure4);
-    measures.push(meal.strMeasure5);
-    measures.push(meal.strMeasure6);
-    measures.push(meal.strMeasure7);
-    measures.push(meal.strMeasure8);
-    measures.push(meal.strMeasure9);
-    measures.push(meal.strMeasure10);
-    measures.push(meal.strMeasure11);
-    measures.push(meal.strMeasure12);
-    measures.push(meal.strMeasure13);
-    measures.push(meal.strMeasure14);
-    measures.push(meal.strMeasure15);
-    measures.push(meal.strMeasure16);
-    measures.push(meal.strMeasure17);
-    measures.push(meal.strMeasure18);
-    measures.push(meal.strMeasure19);
-    measures.push(meal.strMeasure20);
+    return ingredients
+        .map((ingredient, i) => (ingredient && measures[i]) ? `${ingredient} - ${measures[i]}` : "")
+        .filter(Boolean);
+}
 
-    for (let i = 0; i < 20; i++) {
-        if (measures[i] == "" || measures[i] == null || ingredients[i] == "" || ingredients[i] == null) continue;
-        else {
-            ingredients[i] += ` - ${measures[i]}`;
-        }
-    }
-
-    return  ingredients.filter((ingredient) => {
-        console.log(ingredient);
-        return ingredient != "" || ingredient != null;
-    });
-
+// Helper function to create list item
+function createListItem(text) {
+    const listItem = document.createElement("li");
+    listItem.textContent = text;
+    return listItem;
 }

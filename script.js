755177aef576850685e036f3174bb3888df42d1c
Ian Cristino <2224378@slu.edu.ph>
2023-11-19 10:21:32 +0800

    added category filter

--- script.js
+++ script.js
@@ -2,43 +2,61 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const categoryDropdown = document.getElementById('categoryDropdown');
 
 // event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
+categoryDropdown.addEventListener('change', getMealList);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-// get meal list that matches with the ingredients
-function getMealList(){
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
-                        </div>
-                        <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
-                `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+      .then(response => response.json())
+      .then(data => {
+        // Populate the dropdown with categories
+        data.categories.forEach(category => {
+          const option = document.createElement('option');
+          option.value = category.strCategory;
+          option.text = category.strCategory;
+          categoryDropdown.add(option);
+        });
+      })
+      .catch(error => console.error('Error fetching categories:', error));
 
-        mealList.innerHTML = html;
-    });
+// get meal list that matches with the ingredients
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    let selectedCategory = categoryDropdown.value; // Get the selected category
+
+    // Use the selected category in the API request
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}&c=${selectedCategory}`)
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                        <div class="meal-item" data-id="${meal.idMeal}">
+                            <div class="meal-img">
+                                <img src="${meal.strMealThumb}" alt="food">
+                            </div>
+                            <div class="meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a href="#" class="recipe-btn">Get Recipe</a>
+                            </div>
+                        </div>
+                    `;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
+
+            mealList.innerHTML = html;
+        });
 }
 
 
